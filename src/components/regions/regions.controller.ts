f366906a4b2aec3f5d3b27325897d1fce616c13d
Ballbot <5252bb@daum.net>
2023-04-02 22:33:49 +0900

    리전 컨트롤러 추가

---
+++ src/components/regions/regions.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  forwardRef,
+  Get,
+  Inject,
+  NotFoundException,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { RegionAccessTokenGuard } from '../../common/guards/region-access-token.guard';
+import TunnelEntity from '../tunnels/entities/tunnel.entity';
+import { RegionsService } from './regions.service';
+import { TunnelsService } from '../tunnels/services/tunnels.service';
+
+@Controller('regions')
+export class RegionsController {
+  constructor(
+    private readonly regionsService: RegionsService,
+    @Inject(forwardRef(() => TunnelsService))
+    private readonly tunnelsService: TunnelsService,
+  ) {}
+
+  @UseGuards(RegionAccessTokenGuard)
+  @Get('/tunnel/:clientId')
+  async getTunnelByClientId(
+    @Param('clientId') clientId: string,
+  ): Promise<TunnelEntity> {
+    const tunnel = await this.tunnelsService.getTunnelByClientId(clientId);
+
+    if (!tunnel) {
+      throw new NotFoundException();
+    }
+
+    return tunnel;
+  }
+}
